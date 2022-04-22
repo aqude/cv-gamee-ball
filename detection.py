96638de95c1be8d08a696562484c245a59f442db
Sergey Sisharpof <aquadefin@gmail.com>
2022-04-22 23:41:46 +0300

    add click play

--- detection.py
+++ detection.py
@@ -14,11 +14,10 @@ print("Для его остановки: " + "q")
 keyboard.wait('s')
 
 need = cv2.imread("img/need.jpg")
-
 sct = mss.mss()
-monitor = {"top": 598, "left": 958, "width": 649, "height": 841}
-x = 958
-y = 598
+monitor = {"top": 717, "left": 954, "width": 651, "height": 687}
+x = 954
+y = 717
 w = need.shape[1]
 h = need.shape[0]
 
@@ -27,24 +26,38 @@ fps_time = time()
 while True:
     sct_img = np.array(sct.grab(monitor))
     scr_remove = sct_img[:,:,:3]
+    # Мяч
     result = cv2.matchTemplate(scr_remove, need, cv2.TM_CCOEFF_NORMED)
     _, max_val, _, max_loc = cv2.minMaxLoc(result)
-    print(f"Max Val: {max_val} Max Loc: {max_loc}")
-    src = sct_img.copy()
-    if max_val >= .10:
+    
+    # Проверка
+    # print(f"Max Val: {max_val} Max Loc: {max_loc}")
+    src = sct_img.copy() 
+    
+    sleep(.05)
+    if max_val >= .40:
         pyautogui.click(
             x = max_loc[0] + x, 
-            y = max_loc[1] + y 
+            y = max_loc[1] + y
         )
+    else:
+        pyautogui.click(
+            x = 1254, 
+            y = 1379
+        )
+        
+ 
+    
+        
     cv2.rectangle(sct_img, max_loc, (max_loc[0] + w, max_loc[1] + h), (0,255,255), 2)
     cv2.imshow('src', scr_remove)
     cv2.waitKey(1)
     
-    # sleep(.001)
+    sleep(.05)
     if keyboard.is_pressed('q'):
         break
 
-    print('FPS: {}'.format(1 / (time() - fps_time)))
+    #print('FPS: {}'.format(1 / (time() - fps_time)))
     fps_time = time()
      
 
--- tests/fastSrc.py
+++ tests/fastSrc.py
@@ -5,12 +5,12 @@ import numpy as np
 
 with mss.mss() as sct:
     # Область
-    monitor = {"top": 598, "left": 958, "width": 649, "height": 841}
+    monitor = {"top":  717, "left": 954, "width": 651, "height": 687}
+    #monitor = {"top": 953 , "left": 754, "width": 651, "height": 687}
     output = "sct-{top}x{left}_{width}x{height}.png".format(**monitor)
                         
     # Передача данных
-    scr = np.array(sct.grab(monitor))
-    scr_remove = scr[:,:,:3]
+    scr = sct.grab(monitor)
     # Сохранение
-    mss.tools.to_png(scr_remove.rgb, scr_remove.size, output=output)
+    mss.tools.to_png(scr.rgb, scr.size, output=output)
     print(output)
